1a6d9e5608a765a809ce8d8797561b90ed2ed928
Anastasija Lagodzinska <alagodzi@students.cs.ubc.ca>
2025-10-01 22:20:08 -0700

    Added implementation for GameRound class

--- src/main/model/GameRound.java
+++ src/main/model/GameRound.java
@@ -1,38 +1,49 @@
 package model;
 
+import java.util.Random;
+
 // Represents a single round of the game. A round stores the target cake, tracks the user's attempt to recreate a cake
 // and the round outcome.
 public class GameRound {
     public static final int MAX_ROUND_TIME = 30;
+    private static int nextRoundNumber = 1;     // Round number for next new round instance.
 
-    // EFFECTS: Creates a game round with random target cake, default user cake, 
+    private int roundNumber;
+    private Cake targetCake;
+    private Cake userCake;
+    private int completionTime;
+    private boolean isVictory;
+
+    // EFFECTS: Creates a game round with assigned round number, random target cake, default user cake, 
     // zero time, and victory status set to false.
-    public GameRound() {
-        // stub
+    public GameRound(Random random) {
+        this.roundNumber = nextRoundNumber;
+        nextRoundNumber++;
+
+        this.targetCake = new Cake(random);
+        this.userCake = new Cake(); 
+        this.completionTime = 0;
+        this.isVictory = false;
     }
 
     public int getRoundNumber() {
-        return 0;
+        return this.roundNumber;
     }
 
     public boolean getIsVictory() {
-        // stub
-        return false;
+        return this.isVictory;
     }    
 
     public Cake getUserCake() {
-        // stub
-        return null;
+        return this.userCake;
     }
 
     public Cake getTargetCake() {
-        // stub
-        return null;
+        return this.targetCake;
     }
 
     public int getCompletionTime() {
-        // stub
-        return 0;
+        return this.completionTime;
     }
     
     // REQUIRES: The time value between 0 and the maximum allowed round time.
@@ -40,6 +51,9 @@ public class GameRound {
     // EFFECTS: Sets round victory state to true if the target cake and user cake are same
     // otherwise sets to false. Records the time spent to finish the round.
     public void finishRound(int timePlayed) {
-        // stub
+        if (timePlayed >= 0 && timePlayed <= MAX_ROUND_TIME) {
+            this.isVictory = targetCake.compare(userCake);
+            this.completionTime = timePlayed;
+        }
     }
 }
