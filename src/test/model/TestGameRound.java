d7ff531446e5ed0f45788cdacdd410a9ccbd266a
Anastasija Lagodzinska <alagodzi@students.cs.ubc.ca>
2025-10-06 20:38:30 -0700

    Removed time and score logic from GameRound, updated tests for the class

--- src/test/model/TestGameRound.java
+++ src/test/model/TestGameRound.java
@@ -28,20 +28,16 @@ public class TestGameRound {
     @Test
     void testConstructor() {
         assertNotNull(gameRound.getTargetCake());
-        // Check that in the beginning of the round the user have a default cake.
         assertEquals(1, gameRound.getUserCake().getNumberOfTiers());
         assertEquals(CakeColor.WHITE, gameRound.getUserCake().getCakeColor());
         assertEquals(Glaze.NONE, gameRound.getUserCake().getGlaze());
         assertEquals(Topping.NONE, gameRound.getUserCake().getTopping());
         assertEquals(Decoration.NONE, gameRound.getUserCake().getDecoration());
-
-        assertEquals(0, gameRound.getCompletionTime());
-        assertEquals(0, gameRound.getScore());
         assertFalse(gameRound.isVictory());
     }
 
     @Test
-    void testFinishRoundSuccessfulRoundMaxScore() {
+    void testFinishRoundSuccessful() {
         Cake targetCake = gameRound.getTargetCake();
         Cake userCake = gameRound.getUserCake();
         // Match cake
@@ -53,53 +49,10 @@ public class TestGameRound {
 
         assertFalse(gameRound.isVictory());
 
-        gameRound.finishRound(10);
+        gameRound.finishRound();
 
-        assertEquals(GameRound.MAX_SCORE_TIME_LIMIT, gameRound.getCompletionTime());
         assertTrue(gameRound.isVictory());
-        assertEquals(GameRound.MAX_SCORE, gameRound.getScore());
     }
-
-    @Test
-    void testFinishRoundSuccessfulRoundMediumScore() {
-        Cake targetCake = gameRound.getTargetCake();
-        Cake userCake = gameRound.getUserCake();
-        // Match cake
-        userCake.setNumberOfTiers(targetCake.getNumberOfTiers());
-        userCake.setCakeColor(targetCake.getCakeColor());
-        userCake.setGlaze(targetCake.getGlaze());
-        userCake.setTopping(targetCake.getTopping());
-        userCake.setDecoration(targetCake.getDecoration());
-
-        assertFalse(gameRound.isVictory());
-
-        gameRound.finishRound(20);
-
-        assertEquals(GameRound.MEDIUM_SCORE_TIME_LIMIT, gameRound.getCompletionTime());
-        assertTrue(gameRound.isVictory());
-        assertEquals(GameRound.MEDIUM_SCORE, gameRound.getScore());
-    }
-
-    @Test
-    void testFinishRoundTimeSuccessfulMaxAllowedTime() {
-        Cake targetCake = gameRound.getTargetCake();
-        Cake userCake = gameRound.getUserCake();
-        // Match cake
-        userCake.setNumberOfTiers(targetCake.getNumberOfTiers());
-        userCake.setCakeColor(targetCake.getCakeColor());
-        userCake.setGlaze(targetCake.getGlaze());
-        userCake.setTopping(targetCake.getTopping());
-        userCake.setDecoration(targetCake.getDecoration());
-
-        assertFalse(gameRound.isVictory());
-
-        gameRound.finishRound(GameRound.MAX_ROUND_TIME);
-
-        assertEquals(GameRound.MAX_ROUND_TIME, gameRound.getCompletionTime());
-        assertTrue(gameRound.isVictory());
-        assertEquals(GameRound.MIN_SCORE, gameRound.getScore());
-    }
-
 
     @Test
     void testFinishRoundFailedRound() {
@@ -111,32 +64,8 @@ public class TestGameRound {
 
         assertFalse(gameRound.isVictory());
 
-        gameRound.finishRound(20);
+        gameRound.finishRound();
 
-        assertEquals(20, gameRound.getCompletionTime());
         assertFalse(gameRound.isVictory());
-        assertEquals(0, gameRound.getScore());
-    } 
-
-    @Test
-    void testFinishRoundTimeUnderLowerBoundary() {
-        assertFalse(gameRound.isVictory());
-
-        gameRound.finishRound(-1);
-
-        assertEquals(0, gameRound.getCompletionTime());
-        assertFalse(gameRound.isVictory());
-        assertEquals(0, gameRound.getScore());
-    }
-
-    @Test
-    void testFinishRoundTimeOverUpperBoundary() {
-        assertFalse(gameRound.isVictory());
-
-        gameRound.finishRound(GameRound.MAX_ROUND_TIME + 1);
-
-        assertEquals(0, gameRound.getCompletionTime());
-        assertFalse(gameRound.isVictory());
-        assertEquals(0, gameRound.getScore());
     }
 }
