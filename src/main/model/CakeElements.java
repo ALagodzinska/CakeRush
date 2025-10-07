969018be11fe9c62e737a2b94c90db2c1f424a8f
Anastasija Lagodzinska <alagodzi@students.cs.ubc.ca>
2025-10-06 19:41:20 -0700

    Created separate class for cake elements enums

---
+++ src/main/model/CakeElements.java
@@ -0,0 +1,75 @@
+package model;
+
+public class CakeElements {
+    public enum CakeColor { 
+        WHITE, PINK, BLUE, GREEN, YELLOW; 
+
+        public static int length() {
+            return values().length;
+        }
+
+        public static String listAllOptions() {
+            String summary = "";
+
+            for (int i = 1; i <= length(); i++) {
+                summary += i + ": " + values()[ i - 1 ] + " ";
+            }
+
+            return summary;
+        }
+    }
+    
+    public enum Glaze { 
+        NONE, PINK, PURPLE, GREEN, BLUE; 
+    
+        public static int length() {
+            return values().length;
+        }
+
+        public static String listAllOptions() {
+            String summary = "";
+
+            for (int i = 1; i <= length(); i++) {
+                summary += i + ": " + values()[ i - 1 ] + " ";
+            }
+
+            return summary;
+        }
+    }
+
+    public enum Topping { 
+        NONE, SPRINKLES, FRUIT, CANDIES, CREAM; 
+        
+        public static int length() {
+            return values().length;
+        }
+
+        public static String listAllOptions() {
+            String summary = "";
+
+            for (int i = 1; i <= length(); i++) {
+                summary += i + ": " + values()[ i - 1 ] + " ";
+            }
+
+            return summary;
+        }
+    }
+
+    public enum Decoration { 
+        NONE, CANDLE, FLOWER, BALLOON, CARD; 
+        
+        public static int length() {
+            return values().length;
+        }
+
+        public static String listAllOptions() {
+            String summary = "";
+
+            for (int i = 1; i <= length(); i++) {
+                summary += i + ": " + values()[ i - 1 ] + " ";
+            }
+
+            return summary;
+        }
+    }
+}
