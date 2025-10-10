37173587bbf7abc998dcf69d4a842a2f78ecaf95
Anastasija Lagodzinska <alagodzi@students.cs.ubc.ca>
2025-10-09 18:13:41 -0700

    Added documentation for the CakeElements class and changed Gaze option values

--- src/main/model/CakeElements.java
+++ src/main/model/CakeElements.java
@@ -1,16 +1,24 @@
 package model;
 
+// Represents various cake elements. Contains enums for each type of cake element, such as
+// cake color, glaze, toppings and decorations.
 public final class CakeElements {
-    private CakeElements() {    // prevents from creating an instance of the class
+
+    // EFFECTS: Prevents from creating an instance of this class.
+    private CakeElements() {
     }
 
+    // Represents the possible colors of a cake.
     public enum CakeColor { 
         WHITE, PINK, BLUE, GREEN, YELLOW; 
 
+        // EFFECTS: Returns the number of possible color options.
         public static int length() {
             return values().length;
         }
 
+        // EFFECTS: Generates and returns a string with all cake color options,
+        // formatted as a numbered list.
         public static String listAllOptions() {
             String summary = "";
 
@@ -22,13 +30,17 @@ public final class CakeElements {
         }
     }
     
+    // Represents possible glaze options for a cake.
     public enum Glaze { 
-        NONE, PINK, PURPLE, GREEN, BLUE; 
+        NONE, VANILLA, CHOCOLATE, STRAWBERRY, CARAMEL; 
     
+        // EFFECTS: Returns the number of possible glaze options.
         public static int length() {
             return values().length;
         }
 
+        // EFFECTS: Generates and returns a string with all glaze options,
+        // formatted as a numbered list.
         public static String listAllOptions() {
             String summary = "";
 
@@ -40,13 +52,17 @@ public final class CakeElements {
         }
     }
 
+    // Represents possible topping options for a cake.
     public enum Topping { 
         NONE, SPRINKLES, FRUIT, CANDIES, CREAM; 
         
+        // EFFECTS: Returns the number of possible topping options.
         public static int length() {
             return values().length;
         }
 
+        // EFFECTS: Generates and returns a string with all topping options,
+        // formatted as a numbered list.
         public static String listAllOptions() {
             String summary = "";
 
@@ -58,13 +74,17 @@ public final class CakeElements {
         }
     }
 
+    // Represents possible decoration options for a cake.
     public enum Decoration { 
         NONE, CANDLE, FLOWER, BALLOON, CARD; 
         
+        // EFFECTS: Returns the number of possible decoration options.
         public static int length() {
             return values().length;
         }
 
+        // EFFECTS: Generates and returns a string with all decoration options,
+        // formatted as a numbered list.
         public static String listAllOptions() {
             String summary = "";
 
