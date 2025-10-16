4e6a37889e3bc93bd40272a219f4a75af6853e47
Anastasija Lagodzinska <alagodzi@students.cs.ubc.ca>
2025-10-16 12:00:39 -0700

    Added specification of GameLibrary class

---
+++ src/main/model/GameLibrary.java
@@ -0,0 +1,28 @@
+package model;
+
+import java.util.ArrayList;
+
+// Represents a library of all played games. Contains a list of played games.
+public class GameLibrary {
+
+    // EFFECTS: Creates a game library with empty list of games.
+    public GameLibrary() {
+        // stub;
+    }
+
+    // MODIFIES: this
+    // EFFECTS: Creates a new game and adds game to the list of games, inreases the next ID by one.
+    public void createGame() {
+        // stub
+    }
+
+    public ArrayList<GameSession> getPlayedGames() {
+        // stub
+        return null;
+    }
+
+    public int getNextID() {
+        // stub
+        return 0;
+    }
+}
