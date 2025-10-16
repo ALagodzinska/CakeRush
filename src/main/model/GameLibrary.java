84fc0334b1c71436f538ddc4d131c9b2b95c0c95
Anastasija Lagodzinska <alagodzi@students.cs.ubc.ca>
2025-10-16 12:10:19 -0700

    Added implementation for GameLibrary class

--- src/main/model/GameLibrary.java
+++ src/main/model/GameLibrary.java
@@ -3,26 +3,30 @@ package model;
 import java.util.ArrayList;
 
 // Represents a library of all played games. Contains a list of played games.
-public class GameLibrary {
+public class GameLibrary {    
+    private static int nextGameID = 1;          // id for the next game
+    private ArrayList<GameSession> playedGames; // list of all played games
+
 
     // EFFECTS: Creates a game library with empty list of games.
     public GameLibrary() {
-        // stub;
+        playedGames = new ArrayList<>();
     }
 
     // MODIFIES: this
     // EFFECTS: Creates a new game and adds game to the list of games, inreases the next ID by one.
     public void createGame() {
-        // stub
+        GameSession game = new GameSession(nextGameID);
+        nextGameID++;
+
+        playedGames.add(game);
     }
 
     public ArrayList<GameSession> getPlayedGames() {
-        // stub
-        return null;
+        return this.playedGames;
     }
 
     public int getNextID() {
-        // stub
-        return 0;
+        return nextGameID;
     }
 }
