89bc088c8f261ed0a6c5c499b1d39f23574a1d18
Anastasija Lagodzinska <alagodzi@students.cs.ubc.ca>
2025-10-21 14:48:51 -0700

    Added specification of JsonWriter class and Writable interface

---
+++ src/main/persistence/JsonWriter.java
@@ -0,0 +1,45 @@
+package persistence;
+
+import java.io.FileNotFoundException;
+
+import model.GameLibrary;
+
+// Adapted from: 
+//   Project Title: JsonSerializationDemo
+//   Author: CPSC210
+//   Type: source code
+//   URL: https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
+
+// Represents a writer that writes JSON representation of game library to file
+public class JsonWriter {
+
+    // EFFECTS: constructs writer to write to destination file
+    public JsonWriter(String destination) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: opens writer; throws FileNotFoundException if destination file cannot
+    // be opened for writing
+    public void open() throws FileNotFoundException {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes JSON representation of GameLibrary to file
+    public void write(GameLibrary gameLibrary) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: closes writer
+    public void close() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes string to file
+    private void saveToFile(String json) {
+        // stub
+    }
+}
