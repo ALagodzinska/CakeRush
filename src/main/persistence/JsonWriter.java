3123a9443b3c60b529aeb1f367fdeb8c769175c6
Anastasija Lagodzinska <alagodzi@students.cs.ubc.ca>
2025-10-21 20:12:17 -0700

    Added implementation for JsonWriter

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -1,6 +1,10 @@
 package persistence;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+import org.json.JSONObject;
 
 import model.GameLibrary;
 
@@ -12,34 +16,38 @@ import model.GameLibrary;
 
 // Represents a writer that writes JSON representation of game library to file
 public class JsonWriter {
+    private static final int TAB = 4;
+    private PrintWriter writer;
+    private String destination;
 
     // EFFECTS: constructs writer to write to destination file
     public JsonWriter(String destination) {
-        // stub
+        this.destination = destination;
     }
 
     // MODIFIES: this
     // EFFECTS: opens writer; throws FileNotFoundException if destination file cannot
     // be opened for writing
     public void open() throws FileNotFoundException {
-        // stub
+        writer = new PrintWriter(new File(destination));
     }
 
     // MODIFIES: this
     // EFFECTS: writes JSON representation of GameLibrary to file
     public void write(GameLibrary gameLibrary) {
-        // stub
+        JSONObject json = gameLibrary.toJson();
+        saveToFile(json.toString(TAB));
     }
 
     // MODIFIES: this
     // EFFECTS: closes writer
     public void close() {
-        // stub
+        writer.close();
     }
 
     // MODIFIES: this
     // EFFECTS: writes string to file
     private void saveToFile(String json) {
-        // stub
+        writer.print(json);
     }
 }
