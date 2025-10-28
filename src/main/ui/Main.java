8d5945f8c94fc528ccc9a800942892fc1a70e4dc
Anastasija Lagodzinska <alagodzi@students.cs.ubc.ca>
2025-10-28 08:35:44 -0700

    Removed console ui code

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,15 +1,19 @@
 package ui;
 
-import ui.screens.MainScreenHandler;
+import javax.swing.JFrame;
 
 import ca.ubc.cs.ExcludeFromJacocoGeneratedReport;
 
 @ExcludeFromJacocoGeneratedReport
 // Starts the game.
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to the Cake Rush!");
-        MainScreenHandler mh = new MainScreenHandler();
-        mh.runMainMenu();
+    public static void main(String[] args) {
+        // Creating instance of JFrame
+        JFrame frame = new JFrame();
+        frame.setSize(1000, 800);
+        frame.setLocationRelativeTo(null);
+
+        // making the frame visible
+        frame.setVisible(true);
     }
 }
