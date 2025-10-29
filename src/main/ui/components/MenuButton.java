b0eb574d522d07c684d83eddc672e4ed6651dc59
Anastasija Lagodzinska <alagodzi@students.cs.ubc.ca>
2025-10-29 15:54:43 -0700

    Added implementation of two GUI components

---
+++ src/main/ui/components/MenuButton.java
@@ -0,0 +1,23 @@
+package ui.components;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import javax.swing.JButton;
+
+// Styled buttons used in the menus.
+public class MenuButton extends JButton {
+    private static final Dimension btnDimension = new Dimension(300, 40);
+
+    // Creates a styled button with specified text.
+    public MenuButton(String text) {
+        super(text);
+        this.setBackground(Color.PINK);
+        this.setPreferredSize(btnDimension);
+        this.setMaximumSize(btnDimension);
+        this.setMinimumSize(btnDimension);
+        this.setFont(new Font("Arial", Font.BOLD, 20));
+        this.setAlignmentX(Component.CENTER_ALIGNMENT);
+    }
+}
---
+++ src/main/ui/components/Title.java
@@ -0,0 +1,20 @@
+package ui.components;
+
+import java.awt.Component;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+// Styled label used as a title.
+public class Title extends JLabel {
+    // Creates a styled text label with specified text.
+    public Title(String text) {
+        super(text);
+        this.setFont(new Font("Arial", Font.BOLD, 50));
+        this.setHorizontalAlignment(SwingConstants.CENTER);
+        this.setAlignmentX(Component.CENTER_ALIGNMENT);
+        this.setBorder(BorderFactory.createEmptyBorder(10, 10, 5, 10));
+    }
+}
