27fe7f9000942cf786c0a1a119cf109b01752ad8
Anastasija Lagodzinska <alagodzi@students.cs.ubc.ca>
2025-11-03 14:45:19 -0800

    Added temprorary RoundScreen implementation

--- src/main/ui/screens/RoundScreen.java
+++ src/main/ui/screens/RoundScreen.java
@@ -1,18 +1,101 @@
 package ui.screens;
 
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import model.GameRound;
 import model.GameSession;
+import ui.CakeDisplay;
+import ui.MainPanel;
+import ui.components.TimerDisplay;
+import ui.components.Title;
 
 // Represents a round screen.
 public class RoundScreen extends JPanel {
     private GameSession game;
+    private MainPanel mainPanel;
+    private CakeDisplay targetCakeDisplay;
+    private CakeDisplay userCakeDisplay;
+    private TimerDisplay timerDisplay;
+    private Title title;
+    private Timer timer;
+    private int remainingTime;
 
     // EFFECTS: Constructs a round session screen for specified game.
-    public RoundScreen(GameSession game) {
+    public RoundScreen(GameSession game, MainPanel mainPanel) {        
         super();
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         this.game = game;
-        this.add(new JLabel("new round for game " + game.getGameID()));
+        this.mainPanel = mainPanel;      
+        this.title = new Title("MEMORIZE THE CAKE");
+        this.timerDisplay = new TimerDisplay();
+        this.add(title);
+        this.add(timerDisplay);
+        startRound();
     }
+
+    // MODIFIES: this
+    // EFFECTS: starts new round of the game, initializes all cake components on the screen and activates the game.
+    private void startRound() {
+        GameRound round = new GameRound(new Random());
+        targetCakeDisplay = new CakeDisplay(round.getTargetCake());
+        targetCakeDisplay.setAlignmentX(Component.CENTER_ALIGNMENT);
+        userCakeDisplay = new CakeDisplay(round.getUserCake());
+        userCakeDisplay.setAlignmentX(Component.CENTER_ALIGNMENT);
+        this.add(targetCakeDisplay);
+
+        remainingTime = 35;
+        int delay = 1000;
+        timer = new javax.swing.Timer(delay, roundAction());
+
+        timer.start();        
+    }
+
+    // MODIFIES: this
+    // EFFECTS: shows user the target cake and allows user within round time 
+    // to customize user's cake to match target cake.
+    private ActionListener roundAction() {
+        ActionListener action = new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {                
+                if (remainingTime <= 0) {
+                    timer.stop(); 
+                    RoundScreen.this.remove(userCakeDisplay);  
+                    RoundScreen.this.title.setText("RESULTS");
+                    // showComparisonPanel();   // TODO: figure out how to show two at the same time
+                    RoundScreen.this.revalidate(); 
+                    RoundScreen.this.repaint();
+                } else if (remainingTime < 30) {                    
+                    RoundScreen.this.remove(targetCakeDisplay);   
+                    RoundScreen.this.add(userCakeDisplay);    
+                    RoundScreen.this.title.setText("RECREATE THE CAKE");
+                    RoundScreen.this.revalidate(); 
+                    RoundScreen.this.repaint();                
+                }
+                timerDisplay.setTime(remainingTime);
+                remainingTime--;             
+            }
+        };
+
+        return action;
+    }
+
+    // EFFECTS: updates the userCake based on user actions.
+    private void handleCakeChange() {
+        // stub;
+    }
+
+    // TODO: figure out how to show two at the same time
+    // private void showComparisonPanel() {
+    //     JPanel comparisonPanel = new JPanel();
+    //     comparisonPanel.setLayout(new BoxLayout(comparisonPanel, BoxLayout.X_AXIS));
+    //     comparisonPanel.add(targetCakeDisplay);
+    //     comparisonPanel.add(userCakeDisplay);
+    //     this.add(comparisonPanel);
+    // }
 }
